4d05c31f7647c36cfb7759a0fc5b972608160f81
pstet <123316024+pcs03@users.noreply.github.com>
2023-11-27 21:51:31 +0100

    Add datapoint for dvorak progress

--- dvorak-progress/main.py
+++ dvorak-progress/main.py
@@ -2,5 +2,6 @@ from datetime import datetime
 
 # Date as datetime, speed in words per minute
 speeds = [
-    (datetime(year=2023, month=11, day=27), 8)
+    (datetime(year=2023, month=11, day=26), 8),
+    (datetime(year=2023, month=11, day=27), 10),
 ]
