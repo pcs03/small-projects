af0c8d51d3d60be670bf9915338dad7832167f82
pstet <123316024+pcs03@users.noreply.github.com>
2023-11-29 00:19:01 +0100

    Add accuracy measure to dvorak progress

--- dvorak-progress/main.py
+++ dvorak-progress/main.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-# Date as datetime, speed in words per minute
+# Date as datetime, speed in words per minute, accuracy float 0 - 1
 speeds = [
-    (datetime(year=2023, month=11, day=26), 16),
-    (datetime(year=2023, month=11, day=27), 20),
-    (datetime(year=2023, month=11, day=28), 23),
+    (datetime(year=2023, month=11, day=26), 16, None),
+    (datetime(year=2023, month=11, day=27), 20, None),
+    (datetime(year=2023, month=11, day=28), 23, 0.975),
 ]
